7034df9695abc5757a52d968fa45c78997db39b1
Animesh Sinha <animeshsinha1309@gmail.com>
2021-01-15 17:24:21 +0000

    Assignment 1: q1 complete - the first distributed program
    Message passing interface works on the basic stuff beautifully!

---
+++ message-passing/q1.c
@@ -0,0 +1,74 @@
+/* MPI Program Template */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "mpi.h"
+typedef long long int ll;
+
+#define PUSH_TASK 2001
+#define MASTER_PROCESS 0
+
+int main(int argc, char **argv) {
+    int rank, numprocs;
+    MPI_Status status;
+    int start_val, end_val;
+
+    /* start up MPI */
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+    /*synchronize all processes*/
+    MPI_Barrier(MPI_COMM_WORLD);
+    double tbeg = MPI_Wtime();
+
+    /* write your code here */
+    double answer = 0.0;
+
+    if (rank == MASTER_PROCESS) {
+        int n = 0;
+        scanf("%d", &n);
+
+        /* distribute a portion of the bector to each child process */
+        int NUMBERS_PER_PROCESS = (n + numprocs - 1) / numprocs;
+        for (int proc_id = 0; proc_id < numprocs; proc_id++) {
+            start_val = proc_id * NUMBERS_PER_PROCESS + 1;
+            end_val = (proc_id + 1) * NUMBERS_PER_PROCESS;
+            end_val = end_val > n ? n : end_val;
+
+            MPI_Send(&start_val, 1, MPI_INT, proc_id, PUSH_TASK,
+                     MPI_COMM_WORLD);
+            MPI_Send(&end_val, 1, MPI_INT, proc_id, PUSH_TASK, MPI_COMM_WORLD);
+        }
+    }
+    MPI_Recv(&start_val, 1, MPI_INT, MASTER_PROCESS, PUSH_TASK, MPI_COMM_WORLD,
+             &status);
+    MPI_Recv(&end_val, 1, MPI_INT, MASTER_PROCESS, PUSH_TASK, MPI_COMM_WORLD,
+             &status);
+    // printf("@DEBUG: %d got range(%d, %d).\n", rank, start_val, end_val);
+    double sum = 0.0;
+    for (int i = start_val; i <= end_val; i++) {
+        sum += 1.0 / (i * i);
+    }
+    MPI_Reduce(&sum, &answer, 1, MPI_DOUBLE, MPI_SUM, MASTER_PROCESS,
+               MPI_COMM_WORLD);
+
+    /* Compute the time taken and print the answer */
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double elapsedTime = MPI_Wtime() - tbeg;
+    double maxTime;
+    MPI_Reduce(&elapsedTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0,
+               MPI_COMM_WORLD);
+
+    if (rank == MASTER_PROCESS) {
+        printf("%lf\n", answer);
+        // printf("Total time (s): %f\n", maxTime);
+    }
+
+    /* shut down MPI */
+    MPI_Finalize();
+    return 0;
+}
