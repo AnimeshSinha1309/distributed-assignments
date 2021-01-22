7510f456c2f3b13079298d87ddcc1700c23e7e4f
Animesh Sinha <animeshsinha1309@gmail.com>
2021-01-22 07:18:49 +0000

    WIP: Assignment 1: Still working on q3
    I keep going on this, things are now working a lot better.

--- message-passing/q3.cpp
+++ message-passing/q3.cpp
@@ -28,64 +28,32 @@ ostream &operator<<(ostream &out, vector<Type> &vec) {
     return out;
 }
 
-void send_vector(vector<int> v, int proc_id = -1, int sz = -1, int st = -1) {
-    int size = sz == -1 ? v.size() : sz;
-    int *address = st == -1 ? &v[0] : &v[st];
-    MPI_Send(&size, 1, MPI_INT, proc_id, MASTER_PROCESS, MPI_COMM_WORLD);
-    MPI_Send(address, size, MPI_INT, proc_id, MASTER_PROCESS, MPI_COMM_WORLD);
-}
-
-vector<int> recv_vector(int proc_id) {
-    MPI_Status status;
-    vector<int> v;
-    int size = 0;
-    MPI_Recv(&size, 1, MPI_INT, proc_id, PUSH_TASK, MPI_COMM_WORLD, &status);
-    v.resize(size);
-    MPI_Recv(&v[0], size, MPI_INT, proc_id, PUSH_TASK, MPI_COMM_WORLD, &status);
-    return v;
-}
-
 class Graph {
    public:
     vector<vector<int>> g;
-    vector<int> u, v;
     int n, m;
     vector<int> c;
 
     Graph(int n, int m, vector<int> u, vector<int> v) {
-        this->u = u;
-        this->v = v;
         this->n = n;
         this->m = m;
         this->c.assign(m, -1);
         g.resize(m);
         vector<vector<int>> adjacent_edges(n);
         for (int i = 0; i < m; i++) {
-            for (int el : adjacent_edges[u[i]])
-                this->g[i].push_back(el), this->g[el].push_back(i);
-            for (int el : adjacent_edges[v[i]])
-                this->g[i].push_back(el), this->g[el].push_back(i);
             adjacent_edges[u[i]].push_back(i);
             adjacent_edges[v[i]].push_back(i);
         }
-    }
-
-    void send(int proc = -1) {
-        MPI_Bcast(&(this->n), 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
-        MPI_Bcast(&(this->m), 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
-        send_vector(this->u, proc);
-        send_vector(this->v, proc);
-    }
-
-    static Graph input() {
-        int n, m;
-        cin >> n >> m;
-        vector<int> u(m), v(m);
-        for (int i = 0; i < m; i++) {
-            cin >> u[i] >> v[i];
-            u[i]--, v[i]--;
+        for (int node = 0; node < n; node++) {
+            for (int i = 0; i < adjacent_edges[node].size(); i++) {
+                for (int j = 0; j < i; j++) {
+                    int s = adjacent_edges[node][i];
+                    int t = adjacent_edges[node][j];
+                    g[s].push_back(t);
+                    g[t].push_back(s);
+                }
+            }
         }
-        return *(new Graph(n, m, u, v));
     }
 };
 
@@ -103,17 +71,26 @@ int main(int argc, char **argv) {
     double t_start = MPI_Wtime();
     double answer = 0.0;
 
-    if (rank == MASTER_PROCESS) {
-        Graph g = Graph::input();
-        g.send();
-        cout << "Done sending" << endl;
-    }
-    Graph g = g.recv();
-    cout << rank << ": " << g.n << " " << g.m << "\n" << g.g;
+    int n, m;
+    if (rank == MASTER_PROCESS)
+        cin >> n >> m;
+    MPI_Bcast(&n, 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
+    MPI_Bcast(&m, 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
+    vector<int> u(m), v(m);
+    if (rank == MASTER_PROCESS)
+        for (int i = 0; i < m; i++) {
+            cin >> u[i] >> v[i];
+            u[i]--, v[i]--;
+        }
+    MPI_Bcast(&u[0], m, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
+    MPI_Bcast(&v[0], m, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
+    Graph g(n, m, u, v);
+
     int start = ((g.m + numprocs - 1) / numprocs) * rank;
     int end = ((g.m + numprocs - 1) / numprocs) * (rank + 1);
     if (end > g.m)
         end = g.m;
+    cout << "Markers: " << start << ' ' << end << endl;
 
     /* It's coloring time */
     while (true) {
@@ -132,20 +109,25 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        send_vector(g.c, MASTER_PROCESS);
+        MPI_Send(&g.c[0], g.m, MPI_INT, MASTER_PROCESS, PUSH_TASK,
+                 MPI_COMM_WORLD);
+        cout << rank << " Coloring " << g.c;
 
         if (rank == MASTER_PROCESS) {
             for (int proc = 0; proc < numprocs; proc++) {
-                vector<int> update = recv_vector(proc);
+                vector<int> update(g.m);
+                MPI_Status status;
+                MPI_Recv(&update[0], g.m, MPI_INT, proc, PUSH_TASK,
+                         MPI_COMM_WORLD, &status);
+                cout << proc << " recv: " << update;
                 for (int i = 0; i < g.m; i++) {
                     if (g.c[i] == -1)
                         g.c[i] = update[i];
                 }
             }
-            send_vector(g.c);
         }
 
-        g.c = recv_vector(MASTER_PROCESS);
+        MPI_Bcast(&g.c[0], g.m, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
         if (*min_element(g.c.begin(), g.c.end()) > -1)
             break;
     }
